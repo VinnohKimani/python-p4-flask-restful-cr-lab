eacf9cdec5e2af27f2ed4498d1f61a7063be4bda
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-07-03 07:27:25 +0300

    Added Post

--- server/app.py
+++ server/app.py
@@ -22,6 +22,17 @@ class Plants(Resource):
         plants = Plant.query.all()
         return make_response(jsonify([plant.to_dict() for plant in plants]), 200)
 
+    def post(self):
+        data = request.get_json()
+        try:
+            new_plant = Plant(
+                name=data.get("name"), image=data.get("image"), price=data.get("price")
+            )
+            db.session.add(new_plant)
+            db.session.commit()
+            return make_response(jsonify(new_plant.to_dict()), 201)
+        except Exception as e:
+            return make_response({"error": str(e)}, 400)
 
 
 class PlantByID(Resource):
